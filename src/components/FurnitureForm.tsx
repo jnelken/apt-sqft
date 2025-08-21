9060da09386a9d406f8839b31917785b8e3724eb
Jake Nelken <jnelks+gh@gmail.com>
2025-08-21 10:12:01 -0400

    Make constants

--- src/components/FurnitureForm.tsx
+++ src/components/FurnitureForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import { Furniture } from '../types';
-import { FURNITURE_TEMPLATES } from '../data/furnitureTemplates';
+import { FURNITURE_TEMPLATES } from '../lib/constants/furniture.constants';
 import { BaseForm, BaseFormData } from './BaseForm';
 import { formatInitialDimensions } from '../lib/utils/formatInitialDimensions';
 
