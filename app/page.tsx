b7387d424be55493e9d7d9ea4bbea2c029ee094a
Jake Nelken <jnelks+gh@gmail.com>
2025-09-04 12:16:25 -0400

    Add red render flash for dev env

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@
 import dynamic from 'next/dynamic';
 import '@/index.css';
 
+// Import development-only styles
+if (process.env.NODE_ENV === 'development') {
+  import('@/index.dev.css');
+}
+
 const App = dynamic(() => import('@/App'), {
   ssr: false,
   loading: () => <p>Loading...</p>,
